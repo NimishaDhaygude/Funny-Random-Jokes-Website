58e79ff02be8850f6a261d0d287d22f05781267b
Nimisha Dhaygude <123817904+NimishaDhaygude@users.noreply.github.com>
2023-12-13 20:13:43 +0530

    Add files via upload

---
+++ Random_jokes website/script.js
@@ -0,0 +1,305 @@
+let jokes = [ `What do you call a boomerang that won’t come back?
+Ans is = A stick.😆😂`,
+
+`What does a cloud wear under his raincoat?
+Ans is = Thunderwear.😆😂` , 
+
+`Two pickles fell out of a jar onto the flo What did one say to the other?
+Ans is = Dill with it.😆😂` , 
+
+`What time is it when the clock strikes 13?
+Ans is = Time to get a new clock.😆😂` , 
+
+`How does a cucumber become a pickle?
+Ans is = It goes through a jarring experience.😆😂` , 
+
+`What did one toilet say to the other?
+Ans is = You look a bit flushed.😆😂` , 
+
+`What do you think of that new diner on the moon?
+Ans is = Food was good, but there really wasn’t much atmosphere.😆😂` , 
+
+`Why did the dinosaur cross the road?
+Ans is = Because the chicken wasn’t born yet.😆😂` , 
+
+`Why can’t Elsa from Frozen have a balloon?
+Ans is = Because she will "let it go, let it go."😆😂` , 
+
+`What musical instrument is found in the bathroom?
+Ans is = A tuba toothpaste.😆😂` , 
+
+`Why did the kid bring a ladder to school?
+Ans is = Because she wanted to go to high school.😆😂` , 
+
+`What do you call a dog magician?
+Ans is = A labracadabrador.😆😂` , 
+
+`Where would you find an elephant?
+Ans is = The same place you lost her.😆😂` , 
+
+`How do you get a squirrel to like you?
+Ans is = Act like a nut.😆😂` , 
+
+`What do you call two birds in love?
+Ans is = Tweethearts😆😂` , 
+
+`How does a scientist freshen her breath?
+Ans is = With experi-mints.😆😂` , 
+
+`How are false teeth like stars?
+Ans is = They come out at night.😆😂` , 
+
+`What building in your town has the most stories?
+Ans is = The public library.😆😂` , 
+
+`What’s worse than finding a worm in your apple?
+Ans is = Finding half a worm.😆😂` , 
+
+`What is a computer's favorite snack?
+Ans is = Computer chips.😆😂` , 
+
+`What did one volcano say to the other?
+Ans is = I lava you.😆😂` , 
+
+`How do we know that the ocean is friendly?
+Ans is = It waves.😆😂` , 
+
+`What is a tornado’s favorite game to play?
+Ans is = Twister.😆😂` , 
+
+`How does the moon cut his hair?
+Ans is = Eclipse it.😆😂` , 
+
+`How do you talk to a giant?
+Ans is = Use big words.😆😂` , 
+
+`What animal is always at a baseball game?
+Ans is = A bat.😆😂` , 
+
+`What falls in winter but never gets hurt?
+Ans is = Snow.😆😂` , 
+
+`What did the Dalmatian say after lunch?
+Ans is = That hit the spot.😆😂` , 
+
+`Why did the kid cross the playground?
+Ans is = To get to the other slide.😆😂` , 
+
+`What do you call a droid that takes the long way around?
+Ans is = R2 detour.😆😂` , 
+
+`Why did the cookie go to the hospital?
+Ans is = Because he felt crummy.😆😂` , 
+
+`Why was the baby strawberry crying?
+Ans is = Because her mom and dad were in a jam.😆😂` , 
+
+`What did the little corn say to the mama corn?
+Ans is = Where is pop corn?😆😂` , 
+
+`How do you make a lemon drop?
+Ans is = Just let it fall.😆😂` , 
+
+`What did the limestone say to the geologist?
+Ans is = Don’t take me for granite.😆😂` , 
+
+`Why does a seagull fly over the sea?
+Ans is = Because if it flew over the bay, it would be a baygull.😆😂` , 
+
+`What kind of water can’t freeze?
+Ans is = Hot water.😆😂` , 
+
+`What kind of tree fits in your hand?
+Ans is = A palm tree.😆😂` , 
+
+`What do you call a dinosaur that is sleeping?
+Ans is = A dino-snore.😆😂` , 
+
+`What is fast, loud and crunchy?
+Ans is = A rocket chip.😆😂` , 
+
+`Why did the teddy bear say no to dessert?
+Ans is = Because she was stuffed.😆😂` , 
+
+`What has ears but cannot hear?
+Ans is = A cornfield.😆😂` , 
+
+`What did the left eye say to the right eye?
+Ans is = Between us, something smells.😆😂` , 
+
+`What did one plate say to the other plate?
+Ans is = Dinner is on me.😆😂` , 
+
+`Why did the student eat his homework?
+Ans is = Because the teacher told him it was a piece of cake.😆😂` , 
+
+`When you look for something, why is it always in the last place you look?
+Ans is = Because when you find it, you stop looking.😆😂` , 
+
+`What is brown, hairy and wears sunglasses?
+Ans is = A coconut on vacation.😆😂` , 
+
+`What do you say to a rabbit on its birthday?
+Ans is = Hoppy Birthday.😆😂` , 
+
+`What’s the one thing will you get every year on your birthday, guaranteed?
+Ans is = A year older.😆😂` , 
+
+`Why do candles always go on the top of cakes?
+Ans is = Because it's hard to light them from the bottom.😆😂` , 
+
+`What do cakes and baseball teams have in common?
+Ans is = They both need a good batter.😆😂` , 
+
+`What goes up but never comes down?
+Ans is = Your age.😆😂` , 
+
+`What does every birthday end with?
+Ans is = The letter Y.😆😂` , 
+
+`What did the tiger say to her cub on his birthday?
+Ans is = It's roar birthday.😆😂` , 
+
+`Why did the girl put her cake in the freezer?
+Ans is = She wanted to ice it.😆😂` , 
+
+`Does a green candle burn longer than a pink one?
+Ans is = No, they both burn shorter.😆😂` , 
+
+`Why did the little girl hit her birthday cake with a hammer?
+Ans is = It was a pound cake.😆😂` , 
+
+`Why didn't the quarter roll down the hill with the nickel?
+Ans is = Because it had more cents.😆😂` , 
+
+`Why is the obtuse triangle always so frustrated?
+Ans is = Because it’s never right.😆😂` , 
+
+`Why is six afraid of seven?
+Ans is = Because seven eight nine.😆😂` , 
+
+`Why was the equal sign so humble?
+Ans is = Because he wasn’t greater than or less than anyone else.😆😂` , 
+
+`What do you call guys who love math?
+Ans is = Algebros.😆😂` , 
+
+`How do you stay warm in any room?
+Ans is = Go to the corner—it’s always 90 degrees.😆😂` , 
+
+`Why was the fraction nervous about marrying the decimal?
+Ans is = Because he would have to convert.😆😂` , 
+
+`Are monsters good at math?
+Ans is = Not unless you count Dracula.😆😂` , 
+
+`Why was the math book sad?
+Ans is = Because it had too many problems.😆😂` , 
+
+`Why does nobody talk to circles?
+Ans is = Because there’s no point.😆😂` , 
+
+`Why couldn't the pony sing a lullaby?
+Ans is = She was a little horse.😆😂` , 
+
+`What was the first animal in space?
+Ans is = The cow that jumped over the moon.😆😂` , 
+
+`Why don’t elephants chew gum?
+Ans is = They do, just not in public.😆😂` , 
+
+`What did the banana say to the dog?
+Ans is = Bananas can’t talk.😆😂` , 
+
+`How do you make an octopus laugh?
+Ans is = With ten-tickles.😆😂` , 
+
+`What do you call a sleeping bull?
+Ans is = A bull-dozer.😆😂` , 
+
+`How do you fit more pigs on a farm?
+Ans is = Build a sty-scraper.😆😂` , 
+
+`What did the farmer call the cow that had no milk?
+Ans is = An udder failure.😆😂` , 
+
+`What do you call a cow that won't give milk?
+Ans is = A milk dud.😆😂` , 
+
+`Why do fish live in salt water?
+Ans is = Because pepper makes them sneeze.😆😂` , 
+
+`What do you get from a pampered cow?
+Ans is = Spoiled milk.😆😂` , 
+
+`Where do polar bears vote?
+Ans is = The North Poll😆😂` , 
+
+`What sound do porcupines make when they kiss?
+Ans is = Ouch!😆😂` , 
+
+`Why did the snake cross the road?
+Ans is = To get to the other ssside.😆😂` , 
+
+`Why are fish so smart?
+Ans is = Because they live in schools.😆😂` , 
+
+`What did the ocean say to the pirate?
+Ans is = Nothing, it just waved.😆😂` , 
+
+`Why don’t pirates shower before they walk the plank?
+Ans is = Because they’ll just wash up on shore later.😆😂` , 
+
+`What happened when Bluebeard fell overboard in the Red Sea?
+Ans is = He got marooned.😆😂` , 
+
+`How did the pirate get his flag so cheaply?
+Ans is = He bought it on sail.😆😂` , 
+
+`What has 8 legs, 8 arms, and 8 eyes?
+Ans is = 8 pirates.😆😂` , 
+
+`How much does it cost a pirate to get his ears pierced?
+Ans is = About a buck an ear.😆😂` , 
+
+`Why is pirating so addictive?
+Ans is = They say once ye lose yer first hand, ye get hooked.😆😂` , 
+
+`How do pirates know that they are pirates?
+Ans is = They think, therefore they arrr.😆😂` , 
+
+`What is a cat's favorite color?
+Ans is = Purrr-ple.😆😂` , 
+
+`What song does a cat like best?
+Ans is = Three Blind Mice.😆😂` , 
+
+`Where did the school kittens go for their field trip?
+Ans is = To the mew-seum.😆😂` , 
+
+`What kind of kitten works for the Red Cross?
+Ans is = A first-aid kit😆😂.` ]
+
+let r = 0
+// console.log(r)
+let l = jokes.length
+// console.log(l)
+// let random = Math.round((r + (l - r)) * Math.random())
+let random = Math.floor((Math.random()*(jokes.length-1)))
+console.log(random)
+// let random_joke = jokes [random]
+// console.log(random_joke )
+let j = document.getElementById("joke")
+j.innerHTML = jokes[random];
+
+function display_joke(random){
+    j.style.display = "flex";
+    return random;
+ }
+
+// let button_down = document.getElementById("btn")
+// button_down.addEventListener("onclick", display_joke())
+
+
+
+
